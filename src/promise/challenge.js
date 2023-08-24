7a066ed063f5624c9be36402926f8c3dfd04a1ff
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-23 23:04:32 -0600

    Fetch

---
+++ src/promise/challenge.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi){
+    return fetch(urlApi);
+}
+
+// fetchData(`${API}/products`)
+//   .then(response => response.json())
+//   .then(products => {
+//     console.log(products);
+//   })
+//   .then(()=>{
+//     console.log('Hola');
+//   })
+//   .catch( error => console.log(error));
+
+fetchData(`${API}/products`)
+    // traer todos los productos
+   .then(response => response.json())
+   .then(products => {
+        console.log(products);
+        return fetchData(`${API}/products/${products[0].id}`)
+   })
+   //traer un producto
+   .then(response => response.json())
+   .then( product => {
+    console.log(product.title);
+    return fetchData(`${API}/categories/${product.category.id}`)
+   })
+   //traer la categoría 
+   .then(response => response.json())
+   .then(category => {
+      console.log(category.name);
+   })
+   .catch(error => console.log(error))
+   .finally(() =>console.log('Finally'));
